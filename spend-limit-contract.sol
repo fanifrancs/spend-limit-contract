b76bc85284adcb997b82209f14871938270d5946
fanikufran6@gmail.com <fanifrancs>
2025-12-24 19:31:22 +0100

    add events

--- spend-limit-contract.sol
+++ spend-limit-contract.sol
@@ -7,6 +7,11 @@ contract AllowanceWallet {
 
     mapping(address => uint256) public allowance;
 
+    // EVENTS
+    event Deposit(address indexed from, uint256 amount);
+    event AllowanceSet(address indexed spender, uint256 amount);
+    event Spent(address indexed spender, uint256 amount);
+
     constructor() {
         owner = msg.sender;
     }
@@ -19,13 +24,16 @@ contract AllowanceWallet {
     //In Solidity, a function can be marked as payable to receive Ether.
     //  If the function body is empty, it simply accepts the Ether sent
     //  to it without any additional logic.
-    function deposit() public payable {}
+    function deposit() public payable {
+        emit Deposit(msg.sender, msg.value);
+    }
 
     function setAllowance(address _spender, uint256 _amount)
         public
         onlyOwner
     {
         allowance[_spender] = _amount;
+        emit AllowanceSet(_spender, _amount);
     }
 
     function spend(uint256 _amount) public {
@@ -34,6 +42,7 @@ contract AllowanceWallet {
 
         allowance[msg.sender] -= _amount;
         payable(msg.sender).transfer(_amount);
+        emit Spent(msg.sender, _amount);
     }
 
     function getBalance() public view returns (uint256) {
