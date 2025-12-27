c921eba62a5874b1cb92733c8eb37e0e399ef9c4
fanikufran6@gmail.com <fanifrancs>
2025-12-27 23:31:35 +0100

    pause/unpause

--- spend-limit-contract.sol
+++ spend-limit-contract.sol
@@ -4,6 +4,9 @@ pragma solidity ^0.8.0;
 contract AllowanceWallet {
 
     address public owner;
+    //Pause/unpause system acts as an emergency brake in case of
+    //  bugs/exploits by restricting calls to functions
+    bool public paused;
 
     mapping(address => uint256) public allowance;
 
@@ -11,9 +14,12 @@ contract AllowanceWallet {
     event Deposit(address indexed from, uint256 amount);
     event AllowanceSet(address indexed spender, uint256 amount);
     event Spent(address indexed spender, uint256 amount);
+    event Paused();
+    event Unpaused();
 
     constructor() {
         owner = msg.sender;
+        paused = false;
     }
 
     modifier onlyOwner() {
@@ -21,10 +27,28 @@ contract AllowanceWallet {
         _;
     }
 
+    //modifier for the pause/unpause system
+    modifier whenNotPaused() {
+        require(!paused, "Contract paused");
+        _;
+    }
+
+    //toggle functions for pause/unpause boolean
+    function pause() external onlyOwner {
+        paused = true;
+        emit Paused();
+    }
+
+    function unpause() external onlyOwner {
+        paused = false;
+        emit Unpaused();
+    }
+
     //In Solidity, a function can be marked as payable to receive Ether.
     //  If the function body is empty, it simply accepts the Ether sent
-    //  to it without any additional logic.
-    function deposit() public payable {
+    //  to it without any additional logic. Right below, no special logic
+    //  but the emission of the deposit event.
+    function deposit() public payable whenNotPaused {
         emit Deposit(msg.sender, msg.value);
     }
 
@@ -36,7 +60,7 @@ contract AllowanceWallet {
         emit AllowanceSet(_spender, _amount);
     }
 
-    function spend(uint256 _amount) public {
+    function spend(uint256 _amount) public whenNotPaused {
         require(allowance[msg.sender] >= _amount, "Allowance exceeded");
         require(address(this).balance >= _amount, "Insufficient contract balance");
 
